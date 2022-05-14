4c402f36edb0342c3ec588f52e49a0e7aadcb26a
GSoaresgama <gabrielsgamma@gmail.com>
2022-05-14 18:16:22 -0300

    preprocess

---
+++ preprocess.py
@@ -0,0 +1,82 @@
+import os
+import sys
+from glob import glob
+from tkinter import END
+import cv2 as cv
+from PIL import Image
+import numpy as np
+import pydicom
+import pandas as pd
+from tqdm import tqdm
+import SimpleITK as sitk
+
+from skullScrip import skullStripping
+
+import time
+
+dataset_path = "/home/gama/Documentos/datasets/MRI_AX/"
+csv_path = dataset_path + "MRI_AX_5_14_2022.csv"
+
+csv_file = pd.read_csv(csv_path)
+
+info = [csv_file["Image Data ID"], csv_file["Group"]]
+
+# get all files
+files = sorted(glob(dataset_path + "ADNI/*/*/*/*/*.dcm"))
+print("number of files:", len(files))
+
+print("removing calibration scans...")
+files = [k for k in files if "/Calibration_Scan/" not in k]
+print("number of files:", len(files))
+
+new_files = files
+
+png_dir = dataset_path + "ADNI_PNG/"
+os.makedirs(png_dir, exist_ok=True)
+
+id = 0
+for file in tqdm(new_files):
+    id = file.split("_")[-1][:-4]
+    idx = csv_file.index[csv_file["Image Data ID"] == id].to_list()[0]
+    filename = (
+        png_dir + file.split("/")[-1][:-4] + "_" + csv_file["Group"][idx] + ".png"
+    )
+
+    # converts to png
+    # start = time.time()
+    ds = pydicom.dcmread(file)
+    new_image = ds.pixel_array.astype(float)
+    scaled_image = (np.maximum(new_image, 0) / new_image.max()) * 255.0
+    scaled_image = np.uint8(scaled_image)
+    # end = time.time()
+
+    # print("dcm to png:", end - start)
+
+    # start = time.time()
+    img = skullStripping(scaled_image)
+    # end = time.time()
+    # print("skull:", end - start)
+
+    # bias process
+    # start = time.time()
+    imgSitk = sitk.GetImageFromArray(img.astype(np.float32))
+    corrector = sitk.N4BiasFieldCorrectionImageFilter()
+    corrected_image = corrector.Execute(imgSitk)
+    log_bias_field = corrector.GetLogBiasFieldAsImage(imgSitk)
+    corrected_image_full_resolution = imgSitk / sitk.Exp(log_bias_field)
+
+    imgSitk = sitk.GetArrayFromImage(corrected_image_full_resolution)
+    # end = time.time()
+    # print("bias:", end - start)
+
+    # converts to rgb
+    # start = time.time()
+    rgb_img = np.zeros((3, imgSitk.shape[0], imgSitk.shape[1]))
+    img[::] = imgSitk
+    rgb_img = np.transpose(rgb_img, [1, 2, 0])
+    # end = time.time()
+
+    # print("2rgb:", end - start)
+
+    # saves image
+    cv.imwrite(filename, rgb_img)
---
+++ skullScrip.py
@@ -0,0 +1,216 @@
+from PIL import Image
+from scipy import signal as sci
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.morphology import binary_fill_holes, binary_opening
+import matplotlib.image as mpimg
+from skimage import morphology as morph
+from scipy import ndimage as nd
+import sys, os
+from skimage.filters import (
+    threshold_otsu,
+    threshold_isodata,
+    threshold_li,
+    threshold_local,
+    threshold_minimum,
+)
+from skimage import feature
+from skimage.filters import threshold_local as threshold_adaptative
+import heapq
+import matplotlib.patches as mpatches
+from skimage.morphology import convex_hull_image
+import pandas as pd
+import seaborn as sns
+from scipy.signal import convolve2d as sc_conv
+from matplotlib.colors import colorConverter
+import matplotlib as mpl
+import time
+import cv2 as cv
+
+
+def gaussian(s, mu, cov):
+    d = len(s)  # dimension
+    n = np.prod(s)  # n. of samples (pixels)
+    x = np.indices(s).reshape((d, n))
+    xc = x - mu
+    k = 1.0 * xc * np.dot(np.linalg.inv(cov), xc)
+    k = np.sum(k, axis=0)  # the sum is only applied to the rows
+    g = (1.0 / ((2 * np.pi) ** (d / 2.0) * np.sqrt(np.linalg.det(cov)))) * np.exp(
+        -1.0 / 2 * k
+    )
+    return g.reshape(s)
+
+
+def magnitude_sobel(image):
+    Sv = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    Sh = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    fv = sc_conv(image, Sv, mode="same")
+    fh = sc_conv(image, Sh, mode="same")
+    return np.sqrt(fh**2 + fv**2)
+
+
+def save_without_spacing(nome, fig):
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import NullLocator
+
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.margins(0, 0)
+    plt.gca().xaxis.set_major_locator(NullLocator())
+    plt.gca().yaxis.set_major_locator(NullLocator())
+    plt.savefig(nome, bbox_inches="tight", pad_inches=0, dpi=fig.dpi)
+
+
+def three_segments(xa, xb, ya, yb):
+    parte1 = np.floor(np.arange(0, ya, ya / (xa + 1)))
+    parte2 = np.floor(np.arange(ya, yb, (yb - ya) / (xb - xa)))
+    parte3 = np.floor(np.arange(yb, 255, (255 - yb) / (255 - xb)))
+    mascara = np.hstack((parte1, parte2, parte3))
+    return mascara
+
+
+def remove_ruido_fundo(imagem, Ti=0):
+    if Ti == 0:
+        Ti = np.mean(imagem)
+    imagem_filtrada = sci.medfilt(imagem, [3, 3])
+    ti = imagem_filtrada > Ti
+    mascara = binary_fill_holes(ti)
+
+    return [Ti, imagem * mascara]
+
+
+def ajuste_contraste(img):
+    A = img
+    L = np.max(A.ravel())
+    B = (L - 1) - A
+    SE1 = morph.ball(3)[3]
+    C = morph.closing(B, SE1)
+    L = np.max(C.ravel())
+    D = (L - 1) - C
+    Adiff = A - D
+    return np.clip(A + Adiff, 0, 255)
+
+
+def binariza(imagem, Cerebro=True):
+    if Cerebro:
+        t = threshold_otsu(imagem)
+    else:  # Crânio
+        t = 0
+    imagem_binaria = imagem.copy()
+    imagem_binaria[imagem_binaria <= t] = 0
+    imagem_binaria[imagem_binaria > t] = 1
+    return imagem_binaria
+
+
+def detecta_borda_conexa(imagem_binaria):
+    cranio_convexo = convex_hull_image(
+        binary_fill_holes(imagem_binaria)
+    )  # preenche a imagem
+    borda_cranio = feature.canny(cranio_convexo)  # extrai a borda
+    return borda_cranio
+
+
+def detecta_componente_conexa(imagem_binaria, mostrar_Passos=False):
+    label_aux = morph.label(imagem_binaria)
+    label_ret = morph.label(imagem_binaria)
+    if mostrar_Passos == True:
+        plt.figure()
+        plt.subplot(121)
+        plt.imshow(label_ret, "flag")
+        # plt.title('Componentes Conexas(Labels)')
+        # save_without_spacing("TrabalhoMeninge_7_"+str(0)+"comp.png")
+    valor_disco = 0
+
+    # Passo 1 - Detecção da Borda do Crânio via Canny
+    contorno_cranio_binario = detecta_borda_conexa(imagem_binaria)
+    if mostrar_Passos == True:
+        plt.subplot(122)
+        plt.imshow(contorno_cranio_binario, "gray")
+        plt.title("Detecção Borda Canny")
+        plt.figure()
+
+    # Passo 2 - Detecção da Tonalidade do Fundo
+    tonalidade_fundo = np.argmax(np.bincount(label_ret[0:20, 0:20].ravel()))
+    for ab in range(10):
+        # print(ab)
+        contorno_cranio = label_ret[contorno_cranio_binario == True]
+        [IMX, IMY] = imagem_binaria.shape
+
+        # Passo 3 - Detecção da Parte Central do Cérebro (sem fundo)
+        array_centro = label_ret[
+            IMX // 2 - 25 : IMX // 2 + 25, IMY // 2 - 25 : IMY // 2 + 25
+        ]
+        array_centro = array_centro[array_centro != tonalidade_fundo]
+        max_centro = np.argmax(
+            np.bincount(array_centro.ravel())
+        )  # retorna o pixel mais frequente na imagem array_centro, correspondendo ao valor do cérebro
+
+        # Passo 4 - Verificação da Presença da Intensidade da Borda no Centro
+        if max_centro in contorno_cranio:
+            valor_disco = valor_disco + 1
+            label_ret = morph.opening(label_aux, morph.disk(valor_disco))
+            label_ret = morph.label(label_ret)
+            # plt.imshow(label_ret)
+            # save_without_spacing("TrabalhoMeninge_7_"+str(valor_disco)+"comp.png")
+        else:
+            break
+
+    # print(valor_disco)
+    if valor_disco == 0:
+        label_ret = label_aux
+    return [label_ret, tonalidade_fundo, array_centro]
+
+
+def extrai_maior_componente(LC, tonalidade_fundo, array_centro=0, mostrar_Passos=False):
+    # print(type(array_centro))
+    c1 = LC.copy()
+
+    if mostrar_Passos == True:
+        plt.figure(figsize=(8, 8))
+        plt.subplot(121)
+        plt.imshow(c1, "gray")
+        plt.axis("off")
+        plt.title("Componentes Conexas")
+    hist = np.bincount(LC.ravel())
+    index = heapq.nlargest(1, range(len(hist)), key=hist.__getitem__)[-1]
+
+    # Se a maior componente conexa for o fundo, pegue a segunda maior componente
+    if index == tonalidade_fundo:
+        index = heapq.nlargest(2, range(len(hist)), key=hist.__getitem__)[-1]
+
+    # Rerotulação das componentes conexas baseado na ocorrencia de pixels no centro
+    if type(array_centro) == np.ndarray:
+        matriz_substituicao = np.isin(
+            c1, np.unique(array_centro[array_centro != tonalidade_fundo])
+        )
+        c1_k = convex_hull_image(matriz_substituicao)
+        if mostrar_Passos == True:
+            plt.figure()
+            plt.imshow(matriz_substituicao, "gray")
+            plt.axis("off")
+            # plt.title('Agrupamento Conexo')
+            # save_without_spacing("TrabalhoMeninge_8_Agrupamento.png")
+        return [matriz_substituicao, c1_k]
+        # c1[matriz_substituicao] = index
+
+    c1[c1 == index] = 1000
+    c1[c1 < 1000] = 0
+    c1[c1 == 1000] = 1
+    c1_k = convex_hull_image(c1)
+    return [c1, c1_k]
+
+
+def skullStripping(im1):
+    # im1 = Image.open(fileName)
+    img = np.array(im1)
+    ruido = three_segments(220, 240, 50, 210)
+    ruido = ruido[::-1]
+    [media, img] = remove_ruido_fundo(img)
+    Aenhanced = ajuste_contraste(img)
+    Gbin = binariza(Aenhanced)
+    [LC, tonalidade_fundo, array_centro] = detecta_componente_conexa(Gbin, False)
+    [c1, c1_k] = extrai_maior_componente(LC, tonalidade_fundo, array_centro, False)
+
+    img = img * c1_k
+
+    return img
