5508c1582d0a42de5f8c6f523afac3ad796c4084
GSoaresgama <gabrielsgamma@gmail.com>
2022-05-14 10:56:38 -0300

    v2

--- tools/convert2png.py
+++ tools/convert2png.py
@@ -3,9 +3,17 @@ import os
 from PIL import Image
 import numpy as np
 import pydicom
+import pandas as pd
+from tqdm import tqdm
 
 dataset_path = "/home/gama/Documentos/datasets/MRI_AX/"
-csv = dataset_path + "MRI_AX_5_14_2022.csv"
+csv_path = dataset_path + "MRI_AX_5_14_2022.csv"
+
+csv_file = pd.read_csv(csv_path)
+# print(csv_file.head())
+
+info = [csv_file["Image Data ID"], csv_file["Group"]]
+
 # get all files
 files = sorted(glob(dataset_path + "ADNI/*/*/*/*/*.dcm"))
 print("number of files:", len(files))
@@ -14,50 +22,24 @@ print("removing calibration scans...")
 files = [k for k in files if "/Calibration_Scan/" not in k]
 print("number of files:", len(files))
 
-
-# sub = [k for k in files if "002_S_0295" in k]
-
-# for subt in sub:
-#     print(subt, "\n")
-
-
-split = files[0].split("/")
-
-old_cfg = split[7] + split[8] + split[9]
-old_folder = split[10]
-
 new_files = []
 new_files = files
-
-# print("getting only first foler...")
-# for file in files:
-#     split = file.split("/")
-
-#     cfg = split[7] + split[8] + split[9]
-#     folder = split[10]
-#     # print("config:", cfg)
-#     # print("folder:", folder)
-#     if cfg != old_cfg:
-#         old_folder = folder
-#         old_cfg = cfg
-
-#     if folder != old_folder:
-#         # print("folder diff:", folder)
-#         continue
-
-#     # print("adding file")
-#     new_files.append(file)
-#     # input()
 
 print("number of files:", len(new_files))
 
 png_dir = dataset_path + "ADNI_PNG/"
 os.makedirs(png_dir, exist_ok=True)
 
-# ADNI_006_S_4150_MR_3_Plane_Localizer__br_raw_20110809103218384_1_S117931_I249410 is multiple frames
-
-for file in new_files:
-    print("reading file: ", png_dir + file.split("/")[-1][:-4] + ".png")
+id = 0
+for file in tqdm(new_files):
+    # print("file split: ", file.split("/")[-1][:-4])
+    # print("id split: ", file.split("_")[-1][:-4])
+    id = file.split("_")[-1][:-4]
+    idx = csv_file.index[csv_file["Image Data ID"] == id].to_list()[0]
+    filename = (
+        png_dir + file.split("/")[-1][:-4] + "_" + csv_file["Group"][idx] + ".png"
+    )
+    # print("filename: ", str(filename))
 
     ds = pydicom.dcmread(file)
     new_image = ds.pixel_array.astype(float)
