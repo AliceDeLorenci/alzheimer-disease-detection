f6b04b343e29b14c35a775195f6676b35d5d49ac
GSoaresgama <gabrielsgamma@gmail.com>
2022-06-06 22:51:19 -0300

    commented

--- models/confusionMatrix.py
+++ models/confusionMatrix.py
@@ -19,17 +19,23 @@ import numpy as np
 def plot_confusion_matrix(y_true, y_pred, classes, save=False, path=None):
     pd.options.display.float_format = "{:,.4f}".format
 
+    # calculates confusion matrix and normalize
     con_mat = tf.math.confusion_matrix(labels=y_true, predictions=y_pred).numpy()
     con_mat_norm = np.around(con_mat.astype("float") / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
 
     all = len(y_true)
     
+    # accuracy: (TP+TN)/ALL
     acc = [(all + 2*con_mat[i,i] - np.sum(con_mat[i,:])-np.sum(con_mat[:,i]))/all for i in range(len(classes))]
+    # sensitivity: TP/(TP+FN)
     sen = [con_mat[i,i]/np.sum(y_true==i) for i in range(len(classes))]
+    # specificity: TN/(TN+FP)
     esp = [(all + con_mat[i,i] - np.sum(con_mat[i,:]) - np.sum(con_mat[:,i]))/np.sum(~(y_true==i)) for i in range(len(classes))]
     
+
     results = pd.DataFrame( data = np.array([classes, acc, sen, esp]).T, columns = ["class", "Acurácia" , "Sensitividade", "Especificidade"] )
 
+    # plot heatmap
     con_mat_df = pd.DataFrame(con_mat_norm, index=classes, columns=classes)
 
     figure = plt.figure(figsize=(8, 8))
